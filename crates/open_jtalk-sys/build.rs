83f7eef93cb2877c22e9861df5559ec5e4d842e5
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2022-07-18 19:10:31 +0900

    set compile and link options for x86

--- crates/open_jtalk-sys/build.rs
+++ crates/open_jtalk-sys/build.rs
@@ -3,9 +3,7 @@ fn main() {
     let mut cmake_conf = cmake::Config::new("open_jtalk");
     let target = env::var("TARGET").unwrap();
     let cmake_conf = if target.starts_with("i686") {
-        cmake_conf
-            .define("CMAKE_CXX_FLAGS", "-m32")
-            .define("CMAKE_C_FLAGS", "-m32")
+        cmake_conf.define("OPEN_JTALK_X86", "true")
     } else {
         &mut cmake_conf
     };
