7ab282ccd90fda7c0f960fb873d764537c7736ec
char5742 <f.shuuma@gmail.com>
2023-03-14 00:47:00 +0900

    不要なコメントを削除

--- crates/open_jtalk-sys/src/generated/bindings.rs
+++ crates/open_jtalk-sys/src/generated/bindings.rs
@@ -34,7 +34,6 @@ include!(concat!(
     "/src/generated/windows/x86_64/bindings.rs"
 ));
 
-// aarch-linux-android ではbindgenが通らないためlinuxのを読み込む
 #[cfg(all(target_os = "android", target_arch = "aarch64"))]
 include!(concat!(
     env!("CARGO_MANIFEST_DIR"),
