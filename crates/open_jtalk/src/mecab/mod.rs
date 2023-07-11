759c9b988a7214a1befc7680091435b1015e0b12
sevenc-nanashi <sevenc7c@sevenc7c.com>
2023-07-11 15:43:04 +0900

    Change: load_with_userdicのuserdicをOptionに

--- crates/open_jtalk/src/mecab/mod.rs
+++ crates/open_jtalk/src/mecab/mod.rs
@@ -47,25 +47,26 @@ impl Mecab {
             ))
         }
     }
+
     /// # Panics
-    /// 
+    ///
     /// 次の場合にパニックする。
     ///
     /// - `dic_dir`または`userdic`が`\0`を含む。
     /// - `dic_dir`または`userdic`がUTF-8の文字列ではない。
-    pub fn load_with_userdic(
-        &mut self,
-        dic_dir: impl AsRef<Path>,
-        userdic: impl AsRef<Path>,
-    ) -> bool {
-        let dic_dir = CString::new(dic_dir.as_ref().to_str().unwrap()).unwrap();
-        let userdic = CString::new(userdic.as_ref().to_str().unwrap()).unwrap();
-        unsafe {
-            bool_number_to_bool(open_jtalk_sys::Mecab_load_with_userdic(
-                self.as_raw_ptr(),
-                dic_dir.as_ptr(),
-                userdic.as_ptr(),
-            ))
+    pub fn load_with_userdic(&mut self, dic_dir: &Path, userdic: Option<&Path>) -> bool {
+        if let Some(userdic) = userdic {
+            let dic_dir = CString::new(dic_dir.to_str().unwrap()).unwrap();
+            let userdic = CString::new(userdic.to_str().unwrap()).unwrap();
+            unsafe {
+                bool_number_to_bool(open_jtalk_sys::Mecab_load_with_userdic(
+                    self.as_raw_ptr(),
+                    dic_dir.as_ptr(),
+                    userdic.as_ptr(),
+                ))
+            }
+        } else {
+            self.load(dic_dir)
         }
     }
     pub fn get_feature(&self) -> Option<&MecabFeature> {
