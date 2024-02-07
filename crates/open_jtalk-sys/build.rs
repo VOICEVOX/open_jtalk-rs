8b451566da4ff73b66265f87c7046e961dc1f48b
Ryo Yamashita <qryxip@gmail.com>
2024-02-08 03:27:19 +0900

    bindgenをv0.62.0に上げる

--- crates/open_jtalk-sys/build.rs
+++ crates/open_jtalk-sys/build.rs
@@ -95,7 +95,7 @@ fn generate_bindings(
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
         .size_t_is_usize(true)
         .rustfmt_bindings(true)
-        .rustified_enum("*");
+        .rustified_enum(".*");
     let paths = std::fs::read_dir(include_dir).unwrap();
     for path in paths {
         let path = path.unwrap();
